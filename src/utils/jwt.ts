32887d0843f793645baa9067c0f005a41c7acc55
MrSohailAhmad <ahmadsohail1819@gmail.com>
2025-06-27 03:41:25 +0500

    update jwt access token time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,7 +4,7 @@ const ACCESS_SECRET = process.env.JWT_ACCESS_SECRET || "access_secret";
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "refresh_secret";
 
 export const generateAccessToken = (payload: object) =>
-  jwt.sign(payload, ACCESS_SECRET, { expiresIn: "15m" });
+  jwt.sign(payload, ACCESS_SECRET, { expiresIn: "50m" });
 
 export const generateRefreshToken = (payload: object) =>
   jwt.sign(payload, REFRESH_SECRET, { expiresIn: "7d" });
